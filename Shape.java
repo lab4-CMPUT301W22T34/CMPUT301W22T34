83c3711a8358d09eea6ca81b1886fc73ec5096f7
yxing1 <yxing1@ualberta.ca>
2022-02-04 09:45:26 +0800

    Add Shape

---
+++ Shape.java
@@ -0,0 +1,17 @@
+public abstract class Shape{
+    private int y;
+    private int x;
+
+    public Shape(int y, int x) {
+        this.y = y;
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public int getX() {
+        return x;
+    }
+}
