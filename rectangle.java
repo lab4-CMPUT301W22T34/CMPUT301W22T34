6b24f364e1b0bbd28ca5bfe1f4dd6f5ab4c42fdb
yxing1 <yxing1@ualberta.ca>
2022-02-04 09:43:59 +0800

    rectangle

---
+++ rectangle.java
@@ -0,0 +1,18 @@
+public class Rectangle extends Shape{
+    
+    private int y;
+    private int x;
+
+    public Rectangle(int y, int x) {
+        this.y = y;
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public int getX() {
+        return x;
+    }
+}
