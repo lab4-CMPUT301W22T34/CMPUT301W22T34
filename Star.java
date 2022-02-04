cb693e0282f40681bd151618c8f658cc782eec30
FangzhengLi <fli4@ualberta.ca>
2022-02-04 15:17:03 -0700

    Star

---
+++ Star.java
@@ -0,0 +1,19 @@
+public class Star extends Shape{
+    private int y;
+    private int x;
+    String color = "Gold";
+
+    public Star(int y, int x) {
+        this.y = y;
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public int getX() {
+        return x;
+    }
+}
+
